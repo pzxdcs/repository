a23d0b263ca64a93614153ec60d1a0edf7c05097
zxc <zhangxuecheng8@gmail.com>
2015-12-07 11:43:36 +0800

    add hotsearchVC

---
+++ ecbaby/ecbaby/hotSearchCollectionCell.h
@@ -0,0 +1,13 @@
+//
+//  hotSearchCollectionCell.h
+//  ecbaby
+//
+//  Created by qingyun on 15/12/7.
+//  Copyright © 2015年 张雪城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hotSearchCollectionCell : UICollectionViewCell
+
+@end
