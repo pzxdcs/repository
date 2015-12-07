5e1d5aed6cb3c1893d1c2e121d887e387f37fb60
zxc <zhangxuecheng8@gmail.com>
2015-12-07 11:44:04 +0800

    add hotsearch

--- ecbaby/ecbaby/hotSearchCollectionCell.h
+++ ecbaby/ecbaby/hotSearchCollectionCell.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface hotSearchCollectionCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 
 @end
--- ecbaby/ecbaby/hotSearchVC.m
+++ ecbaby/ecbaby/hotSearchVC.m
@@ -8,9 +8,19 @@
 
 #import "hotSearchVC.h"
 #import "searchResultVC.h"
+#import "AFNetworking.h"
+#import "common.h"
+#import "hotSearchCollectionCell.h"
+#import "searchResultVC.h"
+#define kColorRandom(r,g,b)  [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:.8]
 
-@interface hotSearchVC ()
+
+
+@interface hotSearchVC ()<UICollectionViewDataSource,UICollectionViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textfield;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (strong, nonatomic) NSArray *data;
+
 
 @end
 
@@ -18,7 +28,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    [self loadData];
     // Do any additional setup after loading the view.
+}
+-(void)loadData{
+    NSDictionary *param = @{@"c":@"iosapp",@"a":@"labels"};
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:kBaseURL parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSArray *arr = [NSArray arrayWithArray:responseObject[@"data"]];
+        self.data = arr;
+        [self.collectionView reloadData];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+    }];
 }
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
@@ -29,6 +53,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return self.data.count;
+}
+-(UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    hotSearchCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    int r = arc4random() % 255;
+    int g = arc4random() % 255;
+    int b = arc4random() % 255;
+    cell.backgroundColor = kColorRandom(r,g,b);
+    cell.layer.cornerRadius = 35;
+    cell.nameLabel.text = self.data[indexPath.row][@"labelname"];
+    return cell;
+    
+}
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    searchResultVC *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"searchResult"];
+    [self.navigationController pushViewController:vc animated:YES];
+    vc.keywards = self.data[indexPath.row][@"labelname"];
+}
+
+
 - (IBAction)searchButton:(UIButton *)sender {
     [self.view endEditing:YES];
     [NSTimer scheduledTimerWithTimeInterval:.3f target:self selector:@selector(delayMethod) userInfo:nil repeats:NO];
